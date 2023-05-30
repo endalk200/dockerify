36858248f687438c78ca93c9963a1f5db0725260
endalk200 <eb808826@gmail.com>
2023-05-30 17:50:13 +0300

    chore: added command line argument parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Welcome to dockerify!");
+    let supported_projects: Vec<String> = vec!["node".to_string()];
+
+    let project = std::env::args().nth(1).expect("no project specified");
+
+    if !supported_projects.contains(&project) {
+        println!("{} is not a supported project", project);
+        std::process::exit(1);
+    }
 }
